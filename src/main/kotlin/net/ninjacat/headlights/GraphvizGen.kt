5b66543f8bce6ce4fcc0388dbdbe2c15b68cc9b9
Ole Voronin <ovoronin@sonatype.com>
2020-10-25 11:58:24 -0400

    Add Graphiz dot graph generator
    Replace TreeView with Graphviz-generated diagram
    Add menu option to export graphviz dot file
    Update copyright notices and screenshot

---
+++ src/main/kotlin/net/ninjacat/headlights/GraphvizGen.kt
@@ -0,0 +1,74 @@
+package net.ninjacat.headlights
+
+import net.ninjacat.headlights.antlr.ParseTreeError
+import net.ninjacat.headlights.antlr.ParseTreeNode
+import net.ninjacat.headlights.antlr.ParseTreeTerminal
+
+object GraphvizGen {
+
+    private val displayStyles = mapOf(
+        "bg" to "dimgray",
+        "bg_node" to "azure",
+        "edge_color" to "lightsteelblue",
+        "error_text" to "red",
+        "error_stroke" to "orangered",
+        "error_shape" to "note",
+        "error_style" to "filled",
+        "terminal_text" to "navyblue",
+        "terminal_stroke" to "blue",
+        "terminal_shape" to "box",
+        "terminal_style" to "filled",
+        "rule_text" to "black",
+        "rule_stroke" to "white",
+        "rule_shape" to "box",
+        "rule_style" to "\"filled,rounded\"",
+    )
+
+    private val exportStyles = mapOf(
+        "bg" to "white",
+        "bg_node" to "white",
+        "edge_color" to "black",
+        "error_text" to "red",
+        "error_stroke" to "orangered",
+        "error_shape" to "note",
+        "error_style" to "\"\"",
+        "terminal_text" to "navyblue",
+        "terminal_stroke" to "blue",
+        "terminal_shape" to "box",
+        "terminal_style" to "\"\"",
+        "rule_text" to "black",
+        "rule_stroke" to "black",
+        "rule_shape" to "box",
+        "rule_style" to "rounded",
+    )
+
+    private var activeStyle = exportStyles
+
+    fun generateDotFile(root: ParseTreeNode, export: Boolean = false): String {
+        val nodes = mutableListOf<String>()
+        val edges = mutableListOf<String>()
+        activeStyle = if (export) exportStyles else displayStyles
+
+        processNodes(root, nodes, edges)
+        return "graph ParseTree {\nbgcolor=${activeStyle["bg"]}\n" + nodes.joinToString("\n") + "\n" + edges.joinToString("\n") + "\n}\n"
+    }
+
+    private fun nodeStyle(nodeType: String): String {
+        return "color=${activeStyle[nodeType + "_stroke"]} style=${activeStyle[nodeType+"_style"]} " +
+                "fillcolor=${activeStyle["bg_node"]} fontcolor=${activeStyle[nodeType + "_text"]} " +
+                "shape=${activeStyle[nodeType + "_shape"]}"
+    }
+
+    private fun processNodes(node: ParseTreeNode, nodes: MutableList<String>, edges: MutableList<String>) {
+        when (node) {
+            is ParseTreeError -> nodes.add("\"${node.text}_${node.id}\" [${nodeStyle("error")} label=\"${node.text}\"]")
+            is ParseTreeTerminal -> nodes.add("\"${node.text}_${node.id}\" [${nodeStyle("terminal")} label=\"${node.text}\"]")
+            else -> nodes.add("\"${node.text}_${node.id}\" [${nodeStyle("rule")} label=\"${node.text}\"]")
+        }
+        val edgeIds = node.children.map { "\"${it.text}_${it.id}\"" }.joinToString(" ")
+        if (node.children.isNotEmpty()) {
+            edges.add("\"${node.text}_${node.id}\" -- {$edgeIds} [color=${activeStyle["edge_color"]}]")
+        }
+        node.children.forEach { processNodes(it, nodes, edges) }
+    }
+}
---
+++ src/main/kotlin/net/ninjacat/headlights/ui/GraphvizView.kt
@@ -0,0 +1,33 @@
+package net.ninjacat.headlights.ui
+
+import guru.nidi.graphviz.engine.Format
+import guru.nidi.graphviz.engine.Graphviz
+import guru.nidi.graphviz.parse.Parser
+import javafx.scene.control.ScrollPane
+import javafx.scene.image.Image
+import javafx.scene.image.ImageView
+import net.ninjacat.headlights.GraphvizGen
+import net.ninjacat.headlights.antlr.ParseTreeNode
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+
+class GraphvizView: ScrollPane() {
+
+    init {
+        id = "graphviz_view"
+    }
+
+    fun displayTree(node: ParseTreeNode) {
+        val dot = GraphvizGen.generateDotFile(node)
+        val graph = Parser().read(dot)
+        val baos = ByteArrayOutputStream()
+        Graphviz.fromGraph(graph).render(Format.PNG).toOutputStream(baos)
+        val image = Image(ByteArrayInputStream(baos.toByteArray()))
+
+        content = ImageView(image)
+    }
+
+    fun clear() {
+        content = null
+    }
+}
